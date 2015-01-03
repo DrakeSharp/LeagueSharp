5b2f41e5c5281235a0fce0d97d1ddfab99e9555a
DrakeSharp <krystian1876@gmail.com>
2015-01-03 13:40:19 +0100

    beep antispam

--- Anti_Camper/Program.cs
+++ Anti_Camper/Program.cs
@@ -6,7 +6,6 @@ using System.Threading;
 using LeagueSharp;
 using LeagueSharp.Common;
 using SharpDX;
-using SharpDX.Direct3D9;
 using Color = System.Drawing.Color;
 // ReSharper disable CompareOfFloatsByEqualityOperator
 
@@ -19,12 +18,15 @@ namespace Anti_Camper
             public Vector3 pos;
             public float time;
             public Obj_AI_Hero hero;
+            public bool count;
         }
 
+        static float lastbeep;
         static int delay = 0;
         static float test;
         static MenuItem yy;
         static MenuItem xx;
+        static MenuItem lb;
         static MenuItem sound;
         static int textx;
         static int texty;
@@ -46,11 +48,8 @@ namespace Anti_Camper
         {
             CustomEvents.Game.OnGameLoad += OnGameLoad;
         }
-
         static void OnGameLoad(EventArgs args)
         {
-
-
             if (Utility.Map.GetMap().Type != Utility.Map.MapType.SummonersRift) return;
             Menu = new Menu("Anti_Camper", "anti", true);
             Menu.AddItem(
@@ -60,18 +59,22 @@ namespace Anti_Camper
             new MenuItem("duration", "Display time(seconds)").SetValue(
             new Slider(3, 0, 10)));
 
-            xx=new MenuItem("x", "X").SetValue(
+            xx = new MenuItem("x", "X").SetValue(
                     new Slider(
                         (int)(Drawing.Direct3DDevice.Viewport.Width), 0,
                         Drawing.Direct3DDevice.Viewport.Width));
-            yy= new MenuItem("y", "Y").SetValue(
+            yy = new MenuItem("y", "Y").SetValue(
                     new Slider(
                         (int)(Drawing.Direct3DDevice.Viewport.Height), 0,
                         Drawing.Direct3DDevice.Viewport.Height));
+            lb = new MenuItem("freq", "Sound not more often than every seconds").SetValue(
+            new Slider(0, 0, 40));
+            Menu.AddItem(lb);
             Menu.AddItem(yy);
-            Menu.AddItem(xx); 
+            Menu.AddItem(xx);
             sound = new MenuItem("sound", "Play sound").SetValue(false);
             Menu.AddItem(sound);
+            lb.ValueChanged += Changed;
             xx.ValueChanged += Changed;
             yy.ValueChanged += Changed;
             Menu.AddToMainMenu();
@@ -92,37 +95,35 @@ namespace Anti_Camper
             Game.OnGameProcessPacket += OnProcessPacket;
             Drawing.OnDraw += OnDraw;
         }
-
         private static void Changed(object sender, OnValueChangeEventArgs e)
         {
-            test = Game.Time+5;
+            lastbeep = 0;
+            test = Game.Time + 5;
             textx = xx.GetValue<Slider>().Value;
             texty = yy.GetValue<Slider>().Value;
         }
         private static void OnUpdate(EventArgs args)
         {
             int i = 0;
-
             foreach (Obj_AI_Hero hero in heroes)
             {
                 if (hero.Experience != lastexp[i])
                 {
                     lastgain[i] = hero.Experience - lastexp[i];
                     lastexp[i] = hero.Experience;
-                    if (miniondied[i].X!=0)
+                    if (miniondied[i].X != 0)
                     {
-                        
-                        if (lastgain[i]*1.02f< expectedgain[i])
+                        if (lastgain[i] * 1.02f < expectedgain[i])
                         {
-
                             danger s;
                             s.pos = miniondied[i];
-                            s.time = Game.Time+duration;
+                            s.time = Game.Time + duration;
                             s.hero = hero;
+                            s.count = lastgain[i]/expectedgain[i]<.63f;
+                            Game.PrintChat(lastgain[i] +"  "+ expectedgain[i]);
                             dangers = s;
                             miniondied[i].X = 0;
                         }
-
                     }
                 }
                 if (hero.IsMe)
@@ -131,42 +132,41 @@ namespace Anti_Camper
             }
             if (skip == 500)
             {
-                range=Menu.Item("range").GetValue<Slider>().Value;
+                range = Menu.Item("range").GetValue<Slider>().Value;
                 duration = Menu.Item("duration").GetValue<Slider>().Value;
                 minionz = new List<Obj_AI_Minion>();
-                foreach(Obj_AI_Minion minion in ObjectManager.Get<Obj_AI_Minion>())
+                foreach (Obj_AI_Minion minion in ObjectManager.Get<Obj_AI_Minion>())
                     if (minion.BaseSkinName[4].Equals(x))
                         minionz.Add(minion);
                 skip = 0;
             }
-            
+
             skip++;
         }
 
         private static void OnDraw(EventArgs args)
         {
-
-            if (test > Game.Time) Drawing.DrawText(textx, texty, Color.Red, "Enemy detected");
+            if (test > Game.Time) Drawing.DrawText(textx, texty, Color.Red, "MORE THAN ONE Enemy detected");
             if (dangers.pos.X == 0) return;
-            if (dangers.time < Game.Time || (skip%5 == 0 && dangers.pos.Distance(dangers.hero.Position) > range + 1000))
+            if (dangers.time < Game.Time || (skip % 5 == 0 && dangers.pos.Distance(dangers.hero.Position) > range + 1000))
             {
                 delay = 0;
                 dangers.pos.X = 0;
             }
-
-    if ((int)(Game.Time * 2) % 2 == 1) Drawing.DrawText(textx, texty, Color.Red, "Enemy detected");
+            string g = "";
+            if (dangers.count) g = "MORE THAN ONE ";
+            if ((int)(Game.Time * 2) % 2 == 1) Drawing.DrawText(textx, texty, Color.Red, g+"Enemy detected");
             Drawing.DrawCircle(dangers.pos, 1400, Color.Crimson);
             delay++;
-            if (sound.GetValue<Boolean>()&&delay==10)ThreadPool.QueueUserWorkItem(ignoredState =>
-            {
-
-                using (var player = new SoundPlayer(Path.Combine(Environment.GetFolderPath(
-                    Environment.SpecialFolder.ApplicationData), "LeagueSharp\\Repositories\\E0D6961C\\trunk\\Anti_Camper\\Resources\\b.wav")))
+            if (delay == 10 && sound.GetValue<Boolean>() && lastbeep < Game.Time) ThreadPool.QueueUserWorkItem(ignoredState =>
                 {
-                    player.PlaySync();
-                }
-
-            });
+                    lastbeep = Game.Time + lb.GetValue<Slider>().Value;
+                    using (var player = new SoundPlayer(Path.Combine(Environment.GetFolderPath(
+                          Environment.SpecialFolder.ApplicationData), "LeagueSharp\\Repositories\\E0D6961C\\trunk\\Anti_Camper\\Resources\\b.wav")))
+                    {
+                        player.PlaySync();
+                    }
+                });
         }
         private static void OnProcessPacket(GamePacketEventArgs args)
         {
@@ -174,7 +174,7 @@ namespace Anti_Camper
             int nId = BitConverter.ToInt32(args.PacketData, 2);
             foreach (Obj_AI_Minion min in minionz)
             {
-                if (min.NetworkId != nId||myhero.Distance(min)>range) continue;
+                if (min.NetworkId != nId || myhero.Distance(min) > range) continue;
                 float xx = 0;
                 if (min.BaseSkinName[15].Equals('R'))
                     xx = (float)29.44;
@@ -182,7 +182,7 @@ namespace Anti_Camper
                     xx = (float)58.88;
                 else if (min.BaseSkinName[15].Equals('S'))
                     xx = (float)92;
-                        
+
                 short champcount = 0;
                 foreach (Obj_AI_Hero hero in heroes)
                 {
@@ -192,7 +192,7 @@ namespace Anti_Camper
                     }
                 }
                 if (champcount == 0 || champcount == 5) break;
-                if (champcount > 1) xx=xx*(float)0.657;
+                if (champcount > 1) xx = xx * (float)0.657;
                 if (champcount > 2) xx = xx * (float)0.658;
                 if (champcount > 3) xx = xx * (float)0.658;
                 int i = 0;
